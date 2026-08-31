8fa485b1afb9909a82d060cff828b717ab081120
Venkat-Nithin <venkatnithinm@gmail.com>
2026-08-31 13:59:03 +0530

    feat: implement initial application structure with Material UI theme and interview interface components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,24 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme';
 import Home from './components/Home';
 import Login from './components/Login';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
